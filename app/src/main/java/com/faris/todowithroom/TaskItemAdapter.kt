e4610df789d8c0c2997593bb9d573ba09b2bb7e8
farisrizvanovic <fare.faris2@gmail.com>
2022-05-31 23:19:17 +0200

    Adding CardView with GridLayout!

--- app/src/main/java/com/faris/todowithroom/TaskItemAdapter.kt
+++ app/src/main/java/com/faris/todowithroom/TaskItemAdapter.kt
@@ -1,29 +1,37 @@
 package com.faris.todowithroom
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
     var data = listOf<Task>()
+        @SuppressLint("NotifyDataSetChanged")
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    class TaskItemViewHolder(val rootView: TextView) : RecyclerView.ViewHolder(rootView) {
+    class TaskItemViewHolder(rootView: CardView) : RecyclerView.ViewHolder(rootView) {
+
+        private val taskName  = rootView.findViewById<TextView>(R.id.task_name)
+        private val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val view = layoutInflater
-                    .inflate(R.layout.task_item, parent, false) as TextView
+                    .inflate(R.layout.task_item, parent, false) as CardView
                 return TaskItemViewHolder(view)
             }
         }
 
         fun bind(item: Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
     }
 
